e4e0b6c8e7001fce472d0052488e7bb1265232ee
KilianKochFun <kilian.koch2000@outlook.de>
2025-03-23 12:28:52 +0100

    Sticker gefunden

--- locations.js
+++ locations.js
@@ -1,2 +1,8 @@
 const locations = [
+    {
+      position: [50.77580555555556, 6.075638888888888],
+      image: 'img/StickerFunde/ValentinBäcker.webp',
+      title: 'Gegenüber Kaussen',
+      description: 'Ein Valentin an der Stickerwand Kaussen.'
+    },
   ];
