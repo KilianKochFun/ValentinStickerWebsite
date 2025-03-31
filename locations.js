971bd552ed6aea64a1aebb67594d2c36c905f028
KilianKochFun <kilian.koch2000@outlook.de>
2025-03-31 12:06:55 +0200

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,10 +1,26 @@
 const locations = [
-    {
-      position: [50.77580555555556, 6.075638888888888],
-      image: 'img/StickerFunde/ValentinBäcker.webp',
-      time: new Date("03-23-2024 12:20"),
-      finder: "Kilian",
-      title: 'Gegenüber Kaussen',
-      description: 'Ein Valentin an der Stickerwand Kaussen.'
-    },
-  ];
+  {
+    position: [50.77580555555556, 6.075638888888888],
+    image: 'img/StickerFunde/ValentinBäcker.webp',
+    time: new Date("03-23-2024 12:20"),
+    finder: "Kilian",
+    title: 'Gegenüber Kaussen',
+    description: 'Ein Valentin an der Stickerwand Kaussen.'
+  },
+  {
+    position: [39.444666, -0.389583],
+    image: 'img/StickerFunde/ValenciaCat.webp',
+    time: new Date("03-30-2025 18:16"),
+    finder: "Paul",
+    title: 'FCK Cat',
+    description: 'Valentin reaction to FCK Cat'
+  },
+  {
+    position: [50.779833, 6.076139],
+    image: 'img/StickerFunde/Bunker1.webp',
+    time: new Date("03-23-2025 19:37"),
+    finder: "Tim",
+    title: 'Mathekeller 1',
+    description: 'Valentin Nummer 1'
+  }
+];
